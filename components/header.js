d0d9bd0495834ec24be513da6a8b8c8cc91a1147
vimuths123 <vimuths@yahoo.com>
2023-05-16 23:47:42 +0530

    NEw change

--- components/header.js
+++ components/header.js
@@ -6,17 +6,17 @@ const Header = () => {
             <div className='container py-3 d-flex justify-content-between'>
                 <h2 className='text-white align-self-center'>Forms.ai</h2>
                 <div className="float-right d-flex justify-content-between">
-                    <div class="dropdown">
+                    <div className="dropdown">
                         <button
-                            class="btn btn-secondary menu_black border-0 dropdown-toggle text-secondary
+                            className="btn btn-secondary menu_black border-0 dropdown-toggle text-secondary
                             me-5 mt-1 fw-bold"
                             type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
                             Broswe Forms
                         </button>
-                        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                            <li><a class="dropdown-item" href="#">Action</a></li>
-                            <li><a class="dropdown-item" href="#">Another action</a></li>
-                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                            <li><a className="dropdown-item" href="#">Action</a></li>
+                            <li><a className="dropdown-item" href="#">Another action</a></li>
+                            <li><a className="dropdown-item" href="#">Something else here</a></li>
                         </ul>
                     </div>
                     <a className='text-white mt-2'>Sign up / Login</a>
--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,8 @@ import Header from '../components/header';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import Image from 'next/image';
+import { faAngleRight } from '@fortawesome/free-solid-svg-icons';
+import HomeFooter from '../components/homefooter';
 
 
 const Home = () => {
@@ -20,15 +22,15 @@ const Home = () => {
       <Header />
 
       <div className="container max814">
-        <div class="row">
-          <div class="col text-center">
+        <div className="row">
+          <div className="col text-center">
             <h1 className='text-white my-5'>What form are you looking to create?</h1>
             <div className='position-relative'>
-              <input type="text" class="form-control lh-lg ps-5"
+              <input type="text" className="form-control lh-lg ps-5"
                 placeholder="Enter the name of a form (e.g., “Oregon Lease Agreement”)" />
               <FontAwesomeIcon icon={faSearch} className='position-absolute search_icon' />
             </div>
-            <button type="button" class="btn btn-success my-5 home_btn">
+            <button type="button" className="btn btn-success my-5 home_btn">
               <span>
                 <Image src="/images/icons/Comment.png" alt="My Image" width={16} height={10} className='me-2' />
                 Begin
@@ -36,28 +38,138 @@ const Home = () => {
             </button>
             <div className='d-flex'>
               <hr className='example_hr' />
-              <span className='example_span'>Examples</span>
+              <span className='example_span mb-4'>Examples</span>
               <hr className='example_hr' />
             </div>
 
 
 
-            <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-2 g-lg-3">
-              <div class="col">
-                <div class="dark_gray text-white p-2 rounded">
-                  <span>
-                    <Image src="/images/icons/Comment-notfil.png" alt="My Image" width={16} height={10} className='me-2' />
-                    <span className='text-start'>
-                      California residential lease agreement
-                    </span>
+            <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-2 g-lg-3">
+              <div className="col">
+                <div className="dark_gray text-white p-2 rounded homegriditem">
+                  <span className='d-flex pb-3'>
+                    <Image src="/images/icons/Comment-notfil.png" alt="My Image" width={16}
+                      height={10} className='me-2 cment_nofill' />
+                    <div className='text-start'>
+                      "California residential lease agreement"
+                    </div>
                   </span>
+                  <div className='float-end me-2 usediv rounded'>
+                    <span className='me-2'>Use</span>
+                    <FontAwesomeIcon height={13} icon={faAngleRight} /></div>
                 </div>
               </div>
-              <div class="col">
-                <div class="dark_gray text-white p-2 rounded">Row column</div>
+              <div className="col">
+                <div className="dark_gray text-white p-2 rounded homegriditem">
+                  <span className='d-flex pb-3'>
+                    <Image src="/images/icons/Comment-notfil.png" alt="My Image" width={16}
+                      height={10} className='me-2 cment_nofill' />
+                    <div className='text-start'>
+                      "California residential lease agreement"
+                    </div>
+                  </span>
+                  <div className='float-end me-2 usediv rounded'>
+                    <span className='me-2'>Use</span>
+                    <FontAwesomeIcon height={13} icon={faAngleRight} /></div>
+                </div>
               </div>
-              <div class="col">
-                <div class="dark_gray text-white p-2 rounded">Row column</div>
+              <div className="col">
+                <div className="dark_gray text-white p-2 rounded homegriditem">
+                  <span className='d-flex pb-3'>
+                    <Image src="/images/icons/Comment-notfil.png" alt="My Image" width={16}
+                      height={10} className='me-2 cment_nofill' />
+                    <div className='text-start'>
+                      "California residential lease agreement"
+                    </div>
+                  </span>
+                  <div className='float-end me-2 usediv rounded'>
+                    <span className='me-2'>Use</span>
+                    <FontAwesomeIcon height={13} icon={faAngleRight} /></div>
+                </div>
+              </div>
+              <div className="col">
+                <div className="dark_gray text-white p-2 rounded homegriditem">
+                  <span className='d-flex pb-3'>
+                    <Image src="/images/icons/Comment-notfil.png" alt="My Image" width={16}
+                      height={10} className='me-2 cment_nofill' />
+                    <div className='text-start'>
+                      "California residential lease agreement"
+                    </div>
+                  </span>
+                  <div className='float-end me-2 usediv rounded'>
+                    <span className='me-2'>Use</span>
+                    <FontAwesomeIcon height={13} icon={faAngleRight} /></div>
+                </div>
+              </div>
+              <div className="col">
+                <div className="dark_gray text-white p-2 rounded homegriditem">
+                  <span className='d-flex pb-3'>
+                    <Image src="/images/icons/Comment-notfil.png" alt="My Image" width={16}
+                      height={10} className='me-2 cment_nofill' />
+                    <div className='text-start'>
+                      "California residential lease agreement"
+                    </div>
+                  </span>
+                  <div className='float-end me-2 usediv rounded'>
+                    <span className='me-2'>Use</span>
+                    <FontAwesomeIcon height={13} icon={faAngleRight} /></div>
+                </div>
+              </div>
+              <div className="col">
+                <div className="dark_gray text-white p-2 rounded homegriditem">
+                  <span className='d-flex pb-3'>
+                    <Image src="/images/icons/Comment-notfil.png" alt="My Image" width={16}
+                      height={10} className='me-2 cment_nofill' />
+                    <div className='text-start'>
+                      "California residential lease agreement"
+                    </div>
+                  </span>
+                  <div className='float-end me-2 usediv rounded'>
+                    <span className='me-2'>Use</span>
+                    <FontAwesomeIcon height={13} icon={faAngleRight} /></div>
+                </div>
+              </div>
+              <div className="col">
+                <div className="dark_gray text-white p-2 rounded homegriditem">
+                  <span className='d-flex pb-3'>
+                    <Image src="/images/icons/Comment-notfil.png" alt="My Image" width={16}
+                      height={10} className='me-2 cment_nofill' />
+                    <div className='text-start'>
+                      "California residential lease agreement"
+                    </div>
+                  </span>
+                  <div className='float-end me-2 usediv rounded'>
+                    <span className='me-2'>Use</span>
+                    <FontAwesomeIcon height={13} icon={faAngleRight} /></div>
+                </div>
+              </div>
+              <div className="col">
+                <div className="dark_gray text-white p-2 rounded homegriditem">
+                  <span className='d-flex pb-3'>
+                    <Image src="/images/icons/Comment-notfil.png" alt="My Image" width={16}
+                      height={10} className='me-2 cment_nofill' />
+                    <div className='text-start'>
+                      "California residential lease agreement"
+                    </div>
+                  </span>
+                  <div className='float-end me-2 usediv rounded'>
+                    <span className='me-2'>Use</span>
+                    <FontAwesomeIcon height={13} icon={faAngleRight} /></div>
+                </div>
+              </div>
+              <div className="col">
+                <div className="dark_gray text-white p-2 rounded homegriditem">
+                  <span className='d-flex pb-3'>
+                    <Image src="/images/icons/Comment-notfil.png" alt="My Image" width={16}
+                      height={10} className='me-2 cment_nofill' />
+                    <div className='text-start'>
+                      "California residential lease agreement"
+                    </div>
+                  </span>
+                  <div className='float-end me-2 usediv rounded'>
+                    <span className='me-2'>Use</span>
+                    <FontAwesomeIcon height={13} icon={faAngleRight} /></div>
+                </div>
               </div>
             </div>
 
@@ -66,6 +178,8 @@ const Home = () => {
           </div>
         </div>
       </div>
+
+      <HomeFooter/>
     </div>
   )
 }
