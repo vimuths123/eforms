507894431a9fe2a64e85c0692ff5688a7ade90f3
vimuths123 <vimuths@yahoo.com>
2023-06-07 02:56:56 +0530

    New changes

--- pages/chatgbt_p1.js
+++ pages/chatgbt_p1.js
@@ -16,6 +16,8 @@ const ChatgbtP1 = ({ }) => {
   const [answers, setAnswers] = useState([]);
   const [qcountstate, setQcountstate] = useState(0);
   const [isLoad, setIsload] = useState(false);
+  const [loadQuestions, setLoadQuestions] = useState(false);
+  const [appendedComponents, setAppendedComponents] = useState([]);
 
   const router = useRouter();
 
@@ -23,7 +25,7 @@ const ChatgbtP1 = ({ }) => {
   const textareaRef = useRef(null);
   const qdivRef = useRef(null);
 
-  useEffect(() => {
+  function setInitialValues() {
     setInputValue(localStorage.getItem('inputValue'))
     const storedData = localStorage.getItem('questrionArr');
     const storedArray = JSON.parse(localStorage.getItem('answers'));
@@ -47,8 +49,45 @@ const ChatgbtP1 = ({ }) => {
         }
       }
     }
+  }
 
+  useEffect(() => {
+    setInitialValues()
   }, [])
+
+  async function fetchResult(question) {
+    const response = await fetch(process.env.NEXT_PUBLIC_API_URL + 'api/generate/input', {
+      method: 'POST',
+      body: JSON.stringify({
+        "prompt": question
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    const data = await response.json();
+
+    if (data) {
+      localStorage.setItem('questrionArr', JSON.stringify(data));
+      localStorage.setItem('qcount', 0);
+      localStorage.setItem('answers', JSON.stringify([]));
+      setLoadQuestions(false)
+      setAppendedComponents([])
+      setInitialValues()
+    }
+  }
+
+  useEffect(() => {
+    const { question } = router.query;
+    var tempq = localStorage.getItem('inputValue');
+
+    if (question && question != tempq) {
+      setLoadQuestions(true)
+      localStorage.setItem('inputValue', question);
+      fetchResult(question)
+    }
+  }, [router.query]);
 
 
   useEffect(() => {
@@ -84,7 +123,7 @@ const ChatgbtP1 = ({ }) => {
 
   const sendForm = async (formData) => {
     try {
-      const response = await fetch(process.env.NEXT_PUBLIC_API_URL+'api/form', {
+      const response = await fetch(process.env.NEXT_PUBLIC_API_URL + 'api/form', {
         method: 'POST',
         body: JSON.stringify(formData),
         headers: {
@@ -96,7 +135,7 @@ const ChatgbtP1 = ({ }) => {
         throw new Error(`HTTP error! Status: ${response.status}`);
       }
 
-      if(response.ok){
+      if (response.ok) {
         router.push('/document_preview');
       }
 
@@ -121,21 +160,9 @@ const ChatgbtP1 = ({ }) => {
       localStorage.setItem('qcount', qcount);
       setQcountstate(qcount)
     }
-
-
-
-    // if (result) {
-    //   var form = {
-    //     "form_name": inputValue,
-    //     "qa": [
-    //       { "question": "What is your full name", "answer": "jeff boze" },
-    //       { "question": "What is your date of birth", "answer": "02/03/1800" },
-    //     ]
-    //   }
-    // }
   }
 
-  const [appendedComponents, setAppendedComponents] = useState([]);
+
 
   const handleAppendComponent = (type, content) => {
     if (type == 'question') {
@@ -171,85 +198,104 @@ const ChatgbtP1 = ({ }) => {
       <Header />
 
       <div className="container max991">
-        <div className="text-center mt-5">
-          <h3 className="lh-lg">
-            {inputValue}
-          </h3>
-          <p className="lh-lg mb-3">
-            We’re going to ask you a series of step by step questions to build
-            your agreement.
-          </p>
-          <p className="lh-lg mb-3">{qcountstate} of {(result && result.length) ? result.length : ''} questions answered
-          </p>
-        </div>
-        <div className="row">
-          <div className="col text-center">
-
-            <div className="row row-cols-12 g-4 g-lg-4">
-              <div className="col-md-12">
-                {isLoad ? (
+        {loadQuestions ? (
+          <div className="row mt-5">
+            <div className="col text-center">
+              <div className="row row-cols-12 g-4 g-lg-4">
+                <div className="col-md-12">
                   <div className="dark_gray text-white p-4 pe-3 rounded height200 mb-3 position-relative">
-                    <p>Please wait while we build your document.</p>
+                    <p>Please wait while we load your questions</p>
                     <div className="loader-container">
                       <div className="loader"></div>
                     </div>
                   </div>
-                ) : (
-                  <div className="dark_gray text-white p-4 pe-3 rounded height600 mb-3 position-relative">
-                    <div className='qdiv pe-2' ref={qdivRef}>
-                      <div className="middle-content-box-section-1  p-3 rounded-3">
-                        <div className="row">
-                          <div className="col-1">
-                            <Image src="/images/chat-symbol-Group.svg" alt="My Image" width={30}
-                              height={30} />
-                            {/* // <img src="chat-symbol-Group.svg" /> */}
+                </div>
+              </div>
+            </div>
+          </div>
+        ) : (
+          <div>
+            <div className="text-center mt-5">
+              <h3 className="lh-lg">
+                {inputValue}
+              </h3>
+              <p className="lh-lg mb-3">
+                We’re going to ask you a series of step by step questions to build
+                your agreement.
+              </p>
+              <p className="lh-lg mb-3">{qcountstate} of {(result && result.length) ? result.length : ''} questions answered
+              </p>
+            </div>
+            <div className="row">
+              <div className="col text-center">
+
+                <div className="row row-cols-12 g-4 g-lg-4">
+                  <div className="col-md-12">
+                    {isLoad ? (
+                      <div className="dark_gray text-white p-4 pe-3 rounded height200 mb-3 position-relative">
+                        <p>Please wait while we build your document.</p>
+                        <div className="loader-container">
+                          <div className="loader"></div>
+                        </div>
+                      </div>
+                    ) : (
+                      <div className="dark_gray text-white p-4 pe-3 rounded height600 mb-3 position-relative">
+                        <div className='qdiv pe-2' ref={qdivRef}>
+                          <div className="middle-content-box-section-1  p-3 rounded-3">
+                            <div className="row">
+                              <div className="col-1">
+                                <Image src="/images/chat-symbol-Group.svg" alt="My Image" width={30}
+                                  height={30} />
+                                {/* // <img src="chat-symbol-Group.svg" /> */}
+                              </div>
+                              <div className="col-11">
+                                <p className="text-start">
+                                  Great! I will guide you through the process of creating a <span dangerouslySetInnerHTML={{ __html: inputValue }} /> by asking you a series of questions.
+                                  Please provide accurate and detailed information as you respond
+                                  to the questions. Once we have collected all the necessary
+                                  information, I'll help you create the lease agreement. Let's
+                                  start with the first question. {(result && result.length) ? result[0] : ''}?
+                                </p>
+                              </div>
+                            </div>
                           </div>
-                          <div className="col-11">
-                            <p className="text-start">
-                              Great! I will guide you through the process of creating a <span dangerouslySetInnerHTML={{ __html: inputValue }} /> by asking you a series of questions.
-                              Please provide accurate and detailed information as you respond
-                              to the questions. Once we have collected all the necessary
-                              information, I'll help you create the lease agreement. Let's
-                              start with the first question. {(result && result.length) ? result[0] : ''}?
-                            </p>
+
+
+                          {appendedComponents.map((component, index) => (
+                            <div key={index}>{component}</div>
+                          ))}
+                        </div>
+
+
+                        <div className="middle-content-box-3 bottom-0 position-absolute tempmargin">
+                          <div className='position-relative'>
+                            <Image onClick={submitAnswer} className='position-absolute answer_clk' src="/images/icons/Send.png" alt="My Image" width={20}
+                              height={20} />
+                            <textarea
+                              className="form-control pe-5"
+                              placeholder="Please type your annswer"
+                              id="floatingTextarea"
+                              ref={textareaRef}
+                            >
+                              {/* Please type your annswer */}
+                            </textarea>
                           </div>
                         </div>
                       </div>
+                    )}
 
 
-                      {appendedComponents.map((component, index) => (
-                        <div key={index}>{component}</div>
-                      ))}
-                    </div>
-
-
-                    <div className="middle-content-box-3 bottom-0 position-absolute tempmargin">
-                      <div className='position-relative'>
-                        <Image onClick={submitAnswer} className='position-absolute answer_clk' src="/images/icons/Send.png" alt="My Image" width={20}
-                          height={20} />
-                        <textarea
-                          className="form-control pe-5"
-                          placeholder="Please type your annswer"
-                          id="floatingTextarea"
-                          ref={textareaRef}
-                        >
-                          {/* Please type your annswer */}
-                        </textarea>
-                      </div>
-                    </div>
                   </div>
-                )}
+
+
+                </div>
+
 
 
               </div>
-
-
             </div>
-
-
-
           </div>
-        </div>
+        )}
       </div>
 
     </div >
