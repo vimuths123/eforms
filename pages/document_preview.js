e6ed187cdf0f2522fd2ffa3ff8d94e4a7ddd1dd6
vimuths123 <vimuths@yahoo.com>
2023-06-09 03:56:03 +0530

    New changes

--- pages/document_preview.js
+++ pages/document_preview.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import HeaderBar from "../components/header";
 // import styles from '../styles/Home.module.css';
 import Image from 'next/image';
@@ -8,54 +8,73 @@ import { useRouter } from "next/router";
 const documentPreview = () => {
 
   const router = useRouter();
+  const [inputValue, setInputValue] = useState('')
+  const [appendedComponents, setAppendedComponents] = useState([]);
+
+  useEffect(() => {
+    setInputValue(localStorage.getItem('inputValue'));
+    const storedData = localStorage.getItem('questrionArr');
+    const storedArray = JSON.parse(localStorage.getItem('answers'));
+
+    if (storedData) {
+      const parsedData = JSON.parse(storedData);
+
+      if (storedArray && storedArray.length) {
+        if (parsedData.completion && parsedData.completion.length) {
+          storedArray.map((item, index) => {
+            if (index < 11) {
+              handleAppendComponent('question', parsedData.completion[index])
+              handleAppendComponent('answer', storedArray[index])
+            }
+          });
+        }
+      }
+    }
+  }, [])
+
+  const handleAppendComponent = (type, content) => {
+    if (type == 'question') {
+      var componentToAppend = <p className="question mb-1">{content}</p>;
+    }
+
+    if (type == 'answer') {
+      var componentToAppend = <p className="answer mb-3">{content}</p>;
+    }
+
+    setAppendedComponents(prevComponents => [...prevComponents, componentToAppend]);
+  };
+
 
   return (
     <>
       <HeaderBar />
 
-      {/* body-content */}
-      {/* bg= > #353541 */}
-
       <div className="text-center mt-5">
         <h2 className="lh-lg text-center">Create: California lease agreement</h2>
         <p className="h3 lh-lg">
-        Done! Please sign in or create a free account to download (no payment required)
+          Done! Please sign in or create a free account to download (no payment required)
         </p>
-        <button className="btn btn-primary  btn-lg mb-4 w-200" id="headerBtn-1">Sign up /Login</button>
+        <button className="btn btn-primary  btn-lg mb-4 w-200" id="headerBtn-1">Sign up / Login</button>
       </div>
 
-      <div className="middle-content-box-1 container text-center p-1 rounded-3 d-flex align-content-between flex-wrap w-800">
+      <div className="container mt-2">
+        <div className="row">
+          <div className="col-12 col-md-9 custom-bg mx-auto">
+            <div className="bg-white p-5 position-relative">
+              <h1 className="topic mb-4">{inputValue}</h1>
+              {appendedComponents.map((component, index) => (
+                <div key={index}>{component}</div>
+              ))}
+              <p class="esign">eSign</p>
+              <p class="page-number">Page 1</p>
+              {/* <p className="question mb-1">Question 1:</p>
+              <p className="answer mb-3">Answer 1</p>
+              <p className="question mb-1">Question 2:</p>
+              <p className="answer mb-3">Answer 2</p> */}
 
-        <div className="header-set-collections w-800">
-        <div className=" m-3 p-3 rounded-3">
-          
-          {/* <h2 className="text-center">test</h2> */}
-          {/* <img src="Bitmap.svg" /> */}
-          <div className="row" >
-          <div  className="col-12" id="doct-space-1" style={{display:"grid",justifyContent:"center"}}>
-          <Image
-             src="/images/Bitmap.svg"
-             width={500}
-             height={500}
-             alt="document "
-
-             className="doct-img"
-         />
-
-
-{/* <LeaseForm/> */}
-
-
-</div>
-</div>
+            </div>
+          </div>
         </div>
-
-    
-        </div>
-
-        {/* <div className="middle-content-box-2"></div> */}
-
-        
       </div>
     </>
   );
